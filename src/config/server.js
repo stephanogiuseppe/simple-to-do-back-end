dafbbbbd75173454895f6dae931639a6b29a92fd
Stéphano Giuseppe <stephano.grp@gmail.com>
2019-05-26 15:18:26 -0300

    Add CORS middleware

--- src/config/server.js
+++ src/config/server.js
@@ -2,6 +2,7 @@ const port = 3003
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
+const allowCors = require('./cors')
 
 // options: OptionsUrlencoded;
 const options = {
@@ -10,6 +11,7 @@ const options = {
 
 server.use(bodyParser.urlencoded(options))
 server.use(bodyParser.json())
+server.use(allowCors)
 
 server.listen(port, () => {
   console.log('Back running on port ', port)
